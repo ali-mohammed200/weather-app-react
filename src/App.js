6b6e9412df3cd7f0e7ba389f78b64e0b161f9208
ali-mohammed200 <ali.mohammed200@gmail.com>
2019-03-25 15:26:13 -0400

    setting up the form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Form from './components/Form'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>APP.JS  sdfsdf</h1>
+        <h1>APP.JS  sdfsdfsdsd</h1>
+        <Form />
       </div>
     );
   }
---
+++ src/components/Form.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+class Form extends Component {
+  state = {
+    location: ""
+  }
+
+  writer = (e) => {
+    this.setState({location: e.target.value}, ()=> console.log(this.state.location))
+    this.tester()
+  }
+
+  tester = () => {
+    console.log("yes")
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Form Form Form</h1>
+        <input type="text" name="location" placeholder="Enter a location" value={this.state.location} onChange={this.writer}/>
+        <button>Submit</button>
+      </div>
+    );
+  }
+}
+
+export default Form;
+// Form needs state for its rerender on each user keypress.
+// This will enable us to have a controlled form
